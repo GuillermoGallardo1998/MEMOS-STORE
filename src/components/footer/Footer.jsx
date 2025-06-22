81a789a2780a3226a83d87eaa0b28b3c7c1793e4
Guillermo Gallardo Pino <guillermogallardopino@gmail.com>
2025-06-22 06:52:25 -0500

    Build & deploy actualizado

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,21 +1,18 @@
 import "./Footer.css";
 
-import htmlIcon from "icons/html.png";
-import icoCss from "/icons/css.png";
-
 export default function Footer() {
   return (
     <section className="footer">
-        <div class="footer-transition"></div>
+        <div className="footer-transition"></div>
         <div className="footer-tecnologias">
             <p className="footer-tecnologias-title">Technology that Brings This Experience to Life</p>
             <div className="footer-tecnologias-icones">
-                <img className="footer-tecnologias-icon" src={htmlIcon} alt="logo html 5"/>
-                <img className="footer-tecnologias-icon" src={icoCss} alt="logo css"/>
-                <img className="footer-tecnologias-icon" src="/MEMOS-STORE/public/icons/js.png" alt="logo javascript" />
-                <img className="footer-tecnologias-icon" src="icons/git.png" alt="logo git" />
-                <img className="footer-tecnologias-icon" src="icons/react.png" alt="logo react"/>
-                <img className="footer-tecnologias-icon" src="icons/Vite.png" alt="logo vite"/>
+                <img className="footer-tecnologias-icon" src={`${import.meta.env.BASE_URL}icons/html.png`} alt="logo html 5"/>
+                <img className="footer-tecnologias-icon" src={`${import.meta.env.BASE_URL}icons/css.png`} alt="logo css"/>
+                <img className="footer-tecnologias-icon" src={`${import.meta.env.BASE_URL}icons/js-file.png`} alt="logo javascript" />
+                <img className="footer-tecnologias-icon" src={`${import.meta.env.BASE_URL}icons/git.png`} alt="logo git" />
+                <img className="footer-tecnologias-icon" src={`${import.meta.env.BASE_URL}icons/react.png`} alt="logo react"/>
+                <img className="footer-tecnologias-icon" src={`${import.meta.env.BASE_URL}icons/Vite.png`} alt="logo vite"/>
             </div>
         </div>
         <div className="footer-infoLinks">
@@ -26,16 +23,16 @@ export default function Footer() {
             </ul>
             <div className="footer-Links">
                 <a href="https://www.linkedin.com/in/guillermo-andres-gallardo-pino-30911822a/" target="_blank">
-                    <img src="icons/linkedin.png" alt="Linkedin icon" className="footer_icon"/>
+                    <img src={`${import.meta.env.BASE_URL}icons/linkedin.png`} alt="Linkedin icon" className="footer_icon"/>
                 </a>
                 <a href="https://github.com/GuillermoGallardo1998" target="_blank">
-                    <img src="icons/github.png" alt="Github icon" className="footer_icon"/>
+                    <img src={`${import.meta.env.BASE_URL}icons/github.png`} alt="Github icon" className="footer_icon"/>
                 </a>
                 <a href="https://discordapp.com/users/memo8857" target="_blank">
-                    <img src="icons/discord.png" alt="Discord icon" className="footer_icon"/>
+                    <img src={`${import.meta.env.BASE_URL}icons/discord.png`} alt="Discord icon" className="footer_icon"/>
                 </a>
                 <a href="https://www.instagram.com/just.gallardo.98/" target="_blank">
-                    <img src="icons/instagram.png" alt="Instagram icon" className="footer_icon"/>
+                    <img src={`${import.meta.env.BASE_URL}icons/instagram.png`} alt="Instagram icon" className="footer_icon"/>
                 </a>
             </div>
         </div>
